e42f9bdf7291f46c2907349b6ab97b274bcdaba0
Anwesh43 <anweshthecool0@gmail.com>
2019-03-18 11:24:32 +0530

    adding SideWiseIncreasingBarView class

--- sidewiseincreasingbarview/src/main/java/com/anwesh/uiprojects/sidewiseincreasingbarview/SideWiseIncreasingBarView.kt
+++ sidewiseincreasingbarview/src/main/java/com/anwesh/uiprojects/sidewiseincreasingbarview/SideWiseIncreasingBarView.kt
@@ -51,3 +51,21 @@ fun Canvas.drawSWIBNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class SideWiseIncreasingBarView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
