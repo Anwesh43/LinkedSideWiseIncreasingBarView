8fee1219161a48b396933123d50ffff26d1104ff
Anwesh43 <anweshthecool0@gmail.com>
2019-03-18 23:36:00 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedincreasingbarview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedincreasingbarview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedincreasingbarview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.sidewiseincreasingbarview.SideWiseIncreasingBarView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SideWiseIncreasingBarView.create(this)
     }
 }
