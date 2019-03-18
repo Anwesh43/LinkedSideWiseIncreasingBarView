b3953352e21f9a7e59ff867b7fd240b26aedffe8
Anwesh43 <anweshthecool0@gmail.com>
2019-03-18 11:02:30 +0530

    adding sidewiseincreasingbarview module

--- sidewiseincreasingbarview/src/main/java/com/anwesh/uiprojects/sidewiseincreasingbarview/SideWiseIncreasingBarView.kt
+++ sidewiseincreasingbarview/src/main/java/com/anwesh/uiprojects/sidewiseincreasingbarview/SideWiseIncreasingBarView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.sidewiseincreasingbarview
  * Created by anweshmishra on 18/03/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val rects : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#212121")
